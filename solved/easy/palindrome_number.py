40a83604664342a192aaf6b166bec802f969eb26
Marisa Morris <marisamorris@Marisas-MacBook-Pro.local>
2022-04-26 00:42:29 -0700

    solved

--- solved/easy/palindrome_number.py
+++ solved/easy/palindrome_number.py
@@ -19,11 +19,11 @@ isPalindrome(x)
 # Explanation: 121 reads as 121 from left to right and from right to left.
 
 x = -121
-# isPalindrome(x)
+isPalindrome(x)
 # Output: false
 # Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
 
 x = 10
-# isPalindrome(x)
+isPalindrome(x)
 # Output: false
 # Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
--- solved/medium/reverse_words_in_string.py
+++ solved/medium/reverse_words_in_string.py
@@ -1,4 +1,4 @@
-"""
+""" MEDIUM
 Given an input string s, reverse the order of the words.
 
 A word is defined as a sequence of non-space characters. The words in s will be separated by at least one space.
